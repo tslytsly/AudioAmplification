2568159191abc65dd68f97e5ee614e14f3a2ebe1
Thomas Sealey <tpsealey@gmail.com>
2019-01-11 12:03:32 +0000

    Working with input and output gains

--- sketch.js
+++ sketch.js
@@ -1,14 +1,24 @@
-let time = 0;
+let freq = 0;
 let wave = [];
 let path = [];
 let srcAmp = 20;
 let inptGain;
-let otptGain = 2;
+let otptGain;
 let inptGainSlider;
+let otptGainSlider;
 
 function setup() {
-	createCanvas(600, 400);
+	createCanvas(windowWidth, 400);
 	inptGainSlider = createSlider(0, 10, 2, 0.2);
+	otptGainSlider = createSlider(0, 10, 2, 0.2);
+
+	// fill the array at start
+	for (let i = 0; i < width / 3; i++) {
+		let radius = srcAmp;
+		let y = radius * sin(freq);
+		wave.unshift(y);
+		freq += 0.05;
+	}
 }
 
 function draw() {
@@ -16,7 +26,7 @@ function draw() {
 
 	// calc wave
 	let radius = srcAmp;
-	let y = radius * sin(time);
+	let y = radius * sin(freq);
 	wave.unshift(y);
 
 
@@ -32,7 +42,7 @@ function draw() {
 
 	// draw input wave
 	stroke(255);
-	translate(175, 0);
+	translate(wave.length, 0);
 	beginShape();
 	noFill();
 	for (let i = 0; i < wave.length; i++) {
@@ -43,14 +53,33 @@ function draw() {
 	}
 	endShape();
 
+	// draw input voltage rails
 	stroke(255, 0, 0, 100);
-	line(0, -100, 150, -100);
-	line(0, 100, 150, 100);
+	line(0, -100, wave.length, -100);
+	line(0, 100, wave.length, 100);
 
-	time += 0.05;
+	// draw output wave
+	stroke(255);
+	translate(wave.length, 0);
+	beginShape();
+	noFill();
+	for (let i = 0; i < wave.length; i++) {
+		let y = wave[i] * inptGainSlider.value() * otptGainSlider.value();
+		if (y > 150) y = 150;
+		if (y < -150) y = -150;
+		vertex(i, y);
+	}
+	endShape();
+
+	// draw output voltage rails
+	stroke(0, 0, 255, 100);
+	line(0, -150, wave.length, -150);
+	line(0, 150, wave.length, 150);
+
+	freq += 0.05;
 
 
-	if (wave.length > 150) {
+	if (wave.length > floor(width / 3 - 10)) {
 		wave.pop();
 	}
 }
