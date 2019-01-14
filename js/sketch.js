e47f0239a92fbcf6919210fbf394bb8e47090aa8
Thomas Sealey <tpsealey@gmail.com>
2019-01-14 13:54:25 +0000

    made the noise function better

--- js/sketch.js
+++ js/sketch.js
@@ -7,10 +7,10 @@ let srcAmpSlider;
 let stages = {};
 
 function setup() {
-  createCanvas(windowWidth, 400);
-  srcAmpSlider = createSlider(0, 50, 20, 0.5);
+  createCanvas(windowWidth, 600);
+  srcAmpSlider = createSlider(0, 50, 40, 0.5);
   inptGainSlider = createSlider(0, 10, 2, 0.2);
-  otptGainSlider = createSlider(0, 10, 2, 0.2);
+  otptGainSlider = createSlider(0, 10, 1.5, 0.1);
 
   stages.source = new Stage({
     name: "Source",
@@ -28,6 +28,7 @@ function setup() {
     amplitude: inptGainSlider.value(),
     limit: floor(width / 3 - 10),
     maxVolt: 100,
+    noise: true,
     src: stages.source
   });
 
@@ -37,7 +38,9 @@ function setup() {
     y: floor(height / 2),
     amplitude: otptGainSlider.value(),
     limit: floor(width / 3 - 10),
-    maxVolt: 150,
+    maxVolt: 200,
+    noise: true,
+    noiseLvl: 1.5,
     src: stages.input
   });
 
@@ -54,42 +57,6 @@ function draw() {
   for (const key in stages) {
     stages[key].draw();
   }
-
-  // // draw input wave
-  // stroke(255);
-  // translate(wave.length, 0);
-  // beginShape();
-  // noFill();
-  // for (let i = 0; i < wave.length; i++) {
-  //   let y = wave[i] * inptGainSlider.value();
-  //   if (y > 100) y = 100;
-  //   if (y < -100) y = -100;
-  //   vertex(i, y);
-  // }
-  // endShape();
-
-  // // draw input voltage rails
-  // stroke(255, 0, 0, 100);
-  // line(0, -100, wave.length, -100);
-  // line(0, 100, wave.length, 100);
-
-  // // draw output wave
-  // stroke(255);
-  // translate(wave.length, 0);
-  // beginShape();
-  // noFill();
-  // for (let i = 0; i < wave.length; i++) {
-  //   let y = wave[i] * inptGainSlider.value() * otptGainSlider.value();
-  //   if (y > 150) y = 150;
-  //   if (y < -150) y = -150;
-  //   vertex(i, y);
-  // }
-  // endShape();
-
-  // // draw output voltage rails
-  // stroke(0, 0, 255, 100);
-  // line(0, -150, wave.length, -150);
-  // line(0, 150, wave.length, 150);
 
   freq += 0.05;
 }
--- js/stage.js
+++ js/stage.js
@@ -3,11 +3,14 @@ class Stage {
     this.x = 0;
     this.y = 0;
     this.wave = [];
+    this.noiseArr = [];
     this.limit = 150;
     this.amplitude = 10;
     this.frq = 0;
     this.src = null;
     this.maxVolt = 100;
+    this.noise = false;
+    this.noiseLvl = 0.5;
     for (let opt in opts) {
       this[opt] = opts[opt];
     }
@@ -18,25 +21,46 @@ class Stage {
     this.amplitude = amp;
     this.frq = frq;
 
+    // noise function
+    if (this.noise) {
+      let n = this.noiseLvl * this.amplitude;
+      n *= sin(random(PI, -PI));
+      n = constrain(n, this.maxVolt * -1, this.maxVolt)
+      this.noiseArr.unshift(n);
+    }
+
+    // let n = this.noiseArr[i] * (this.amplitude * 0.2);
+    // n = constrain(n, this.maxVolt * -1, this.maxVolt);
+    // this.noiseArr[i] = n;
+
     // if we have a source, all wave plots must be based on it
     if (this.src !== null) {
       for (let i = 0; i < this.src.wave.length; i++) {
         let y = this.src.wave[i] * this.amplitude;
         y = constrain(y, this.maxVolt * -1, this.maxVolt);
         this.wave[i] = y;
+        if (this.noise) {
+          this.wave[i] += this.noiseArr[i];
+        }
       }
     } else { //otherwise
       let y = this.amplitude * sin(this.frq);
       this.wave.unshift(y);
     }
 
+
     if (this.wave.length > this.limit) {
       this.wave.pop();
+    }
+
+    if (this.noiseArr.length > this.limit) {
+      this.noiseArr.pop();
     }
   }
 
   draw() {
-    // draw it
+    // draw main wave
+    strokeWeight(1);
     push();
     stroke(255);
     translate(this.x, this.y);
@@ -47,21 +71,42 @@ class Stage {
     }
     endShape();
 
+    // draw noise
+    if (this.noise) {
+      push();
+      stroke(255, 100);
+      translate(0, 0);
+      beginShape();
+      noFill();
+      for (let i = 0; i < this.noiseArr.length; i++) {
+        vertex(i, this.noiseArr[i]);
+      }
+      endShape();
+      pop();
+    }
+
     // draw output voltage rails if not the source
     if (this.src) {
       stroke(255, 0, 0, 100);
       line(0, -this.maxVolt, this.limit, -this.maxVolt);
       line(0, this.maxVolt, this.limit, this.maxVolt);
     }
+    pop();
 
     // Draw name if configured
     if (this.name) {
       textSize(24);
       textAlign(CENTER);
       fill(255);
-      text(this.name, this.limit/2, -this.maxVolt - 25);
+      text(this.name, this.x + this.limit / 2, 50);
     }
-    pop();
+
+    // draw vertical lines
+    stroke(255, 100);
+    strokeWeight(4);
+    line(this.x, 0, this.x, height);
+    // line(this.x + this.limit + 2, 0, this.x + this.limit + 2, height);
+
   }
 
   init() {
